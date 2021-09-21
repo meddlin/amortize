24de3a5b74bbc5c9e5bec128a3b8850e1c6d39f2
meddlin <drushing.dev@gmail.com>
2021-09-21 01:11:14 -0500

    Table styling
    - Centered it, I think?
    - Added some color

--- amortize-client/src/components/table.js
+++ amortize-client/src/components/table.js
@@ -1,37 +1,36 @@
-function Table({ tableData }) {
+import './table.css';
 
+function Table({ tableData }) {
     return (
         <div className="Table">
             <h2>Amortization Table</h2>
             <table>
                 <thead>
-                <tr>
-                    <th>Term</th>
-                    <th>Monthly Payment</th>
-                    <th>Principal</th>
-                    <th>Interest</th>
-                    <th>Remain. Pri.</th>
-                    <th>Extra Payment</th>
-                </tr>
+                    <tr>
+                        <th>Term</th>
+                        <th>Monthly Payment</th>
+                        <th>Principal</th>
+                        <th>Interest</th>
+                        <th>Remain. Pri.</th>
+                        <th>Extra Payment</th>
+                    </tr>
                 </thead>
                 <tbody>
-                {
-                tableData && tableData.length > 0 ? 
-                tableData.map( 
-                    a => {
-                        return (
-                            <tr key={a.term}>
-                                <td>{a.term}</td>
-                                <td>{a.monthlyPayment}</td>
-                                <td>{a.principal}</td>
-                                <td>{a.interest}</td>
-                                <td>{a.remainingPrincipal}</td>
-                                <td>{a.extraPayment}</td>
-                            </tr>
-                        )
-                    } 
-                ) : 'No data'
-                }
+                    { tableData && tableData.length > 0 ? 
+                    tableData.map( 
+                        a => {
+                            return (
+                                <tr key={a.term}>
+                                    <td>{a.term}</td>
+                                    <td>{a.monthlyPayment}</td>
+                                    <td>{a.principal}</td>
+                                    <td>{a.interest}</td>
+                                    <td>{a.remainingPrincipal}</td>
+                                    <td>{a.extraPayment}</td>
+                                </tr>
+                            )
+                        } 
+                    ) : 'No data' }
                 </tbody>
             </table>
         </div>
