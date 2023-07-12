647802db2c4098d92900b5567df7fa6b2440fcc2
meddlin <drushing.dev@gmail.com>
2023-07-12 02:03:48 -0500

    started Rolloff component

---
+++ amortize-client/src/components/rolloff.js
@@ -0,0 +1,12 @@
+function Rolloff() {
+    return (
+        <div>
+            <h3>PMI rolloff</h3>
+
+            <div>PMI Cancel Amt: #</div>
+            <div>Term: #</div>
+        </div>
+    );
+};
+
+export default Rolloff;
